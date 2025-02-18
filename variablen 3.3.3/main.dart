b09e7919a21219943480e1219b573ed814a7aeec
Leon Bösche <leonboesche1999@gmail.com>
2025-02-18 10:56:08 +0100

    variablen

---
+++ variablen 3.3.3/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String d = "!";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+
+  // Die Variable d wird doppelt und mit verschiedenen Datentypen verwendet!
+}
