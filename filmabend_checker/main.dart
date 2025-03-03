d55a2b937f936ea4820e9560a2410cae873d1b0e
Leon Bösche <leonboesche1999@gmail.com>
2025-03-03 11:38:23 +0100

    added Aufgabe 2

--- filmabend_checker/main.dart
+++ filmabend_checker/main.dart
@@ -1,3 +1,4 @@
+// Aufgabe 1
 void main() {
   int age = 26;
   bool hasParentalConsent = true;
@@ -7,5 +8,25 @@ void main() {
     print("Erlaubnis erteilt!");
   } else {
     print("Erlaubnis verweigert!");
+  }
+}
+
+// Aufgabe 2
+void main2() {
+  bool isLoggedIn = true;
+  bool isBanned = false;
+  bool isSubscribed = true;
+  int age = 26;
+
+  if (!isLoggedIn) {
+    print("Bitte einloggen!");
+  } else if (isBanned) {
+    print("Account gesperrt!");
+  } else if (!isSubscribed) {
+    print("Bitte abonnieren!");
+  } else if (age < 18) {
+    print("Mindestalter nicht erreicht!");
+  } else {
+    print("Viel Spaß beim Streamen!");
   }
 }
