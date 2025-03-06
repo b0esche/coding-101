0ac9218051da4a66863edff09131ba5894219d8f
Leon Bösche <leonboesche1999@gmail.com>
2025-03-06 16:10:51 +0100

    3.4.5

---
+++ 3.4.5/main.dart
@@ -0,0 +1,26 @@
+List<String> names = [
+  'Julietta',
+  'Benjamino',
+  'Hans-Günther',
+  'Evalinea',
+  'Fiona',
+  'Gregory',
+  'Leonhart'
+];
+
+void main() {
+  for (String x in names) {
+    print(x);
+  }
+}
+
+// List<int> points = [4, 5, 4, 2, 6, 6, 3];
+
+// void main() {
+//   int sum = 0;
+
+//   for (int x in points) {
+//     sum += x;
+//   }
+//   print("Der Durchschnittswert beträgt: ${sum / points.length}");
+// }
