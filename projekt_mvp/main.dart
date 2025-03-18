0134f85f844073a7f3ad87f024808e1f3a553b80
Leon Bösche <leonboesche1999@gmail.com>
2025-03-18 15:01:53 +0100

    tested main

--- projekt_mvp/main.dart
+++ projekt_mvp/main.dart
@@ -1,8 +1,10 @@
 import 'classes/chat_list_item.dart';
 
 void main() {
-  ChatListItem x = new ChatListItem(
-      "einName", "Hi dies ist die letzte Nachricht", "avatarUrl");
+  ChatListItem x = ChatListItem(
+      "einName", "Hi dies ist die letzte Nachricht", "avatarUrl.html");
 
+  x.name = "DJ Bifi";
   print(x.name);
+  print(x.recentMessage);
 }
