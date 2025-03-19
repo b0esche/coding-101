20eabbd089d3321007ff759aec273496177c1cb6
Leon Bösche <leonboesche1999@gmail.com>
2025-03-19 10:56:00 +0100

    defused main

--- projekt_mvp/main.dart
+++ projekt_mvp/main.dart
@@ -2,14 +2,14 @@ import 'classes/chat_list_item.dart';
 
 void main() {
   // Neue Instanz IN Variable erzeugen
-  ChatListItem x = ChatListItem(
-      "einName", "Hi dies ist die letzte Nachricht", "avatarUrl.html");
+//   ChatListItem x = ChatListItem(
+//       "einName", "Hi dies ist die letzte Nachricht", "avatarUrl.html");
 
-// Attributwerte manipulieren und printen
-  x.name = "DJ Bifi";
-  print(x.name);
-  print(x.recentMessage);
+// // Attributwerte manipulieren und printen
+//   x.name = "DJ Bifi";
+//   print(x.name);
+//   print(x.recentMessage);
 
-  // Neue Instanz OHNE Variable erzeugen
-  new ChatListItem("peter", "hallo manni", "www.de");
+//   // Neue Instanz OHNE Variable erzeugen
+//   new ChatListItem("peter", "hallo manni", "www.de");
 }
