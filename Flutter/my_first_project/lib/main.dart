ae294f650d32c75066d38fc083706b7744b79af2
Leon Bösche <leonboesche1999@gmail.com>
2025-03-31 14:16:37 +0200

    2.0

--- Flutter/my_first_project/lib/main.dart
+++ Flutter/my_first_project/lib/main.dart
@@ -10,8 +10,22 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Titel"), centerTitle: true),
+        appBar: AppBar(
+          title: Text(
+            "Titel",
+            style: TextStyle(
+              color: const Color.fromARGB(255, 194, 194, 194),
+              letterSpacing: 2.0,
+              decoration: TextDecoration.underline,
+              decorationColor: const Color.fromARGB(255, 194, 194, 194),
+              fontSize: 24,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 88, 78, 226),
+        ),
         body: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
@@ -36,6 +50,61 @@ class MainApp extends StatelessWidget {
               ),
               SizedBox(height: 16),
               Text("Hier steht der Artikel...", style: TextStyle(fontSize: 16)),
+              Divider(thickness: 2),
+              SizedBox(height: 16),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                spacing: 24,
+
+                children: [
+                  Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 100,
+                        alignment: Alignment.center,
+                        color: Colors.amber,
+                        child: Text("A"),
+                      ),
+                      Positioned(top: 0, child: Text("Text A")),
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Text B"),
+                      Container(
+                        height: 144,
+                        width: 100,
+                        alignment: Alignment.center,
+                        color: const Color.fromARGB(255, 235, 129, 47),
+                        child: Text("B"),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 100,
+                        alignment: Alignment.centerRight,
+                        child: Text("Text C"),
+                      ),
+                      Container(
+                        height: 108,
+                        width: 100,
+                        alignment: Alignment.center,
+                        color: const Color.fromARGB(255, 255, 100, 29),
+                        child: Text("C"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(thickness: 2),
             ],
           ),
         ),
